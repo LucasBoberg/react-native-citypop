02ed57cd0149a8133358181cac71ab7b6f158d6c
Lucas Boberg <lucaspboberg@gmail.com>
2021-12-22 13:36:54 +0100

    Implements autocomplete for Country Search

--- screens/SearchCountryScreen.tsx
+++ screens/SearchCountryScreen.tsx
@@ -1,41 +1,66 @@
 import React, {useState} from 'react';
-import {View} from 'react-native';
+import {FlatList, View} from 'react-native';
+import {Button} from '../components/Button';
+import {NoResult} from '../components/NoResult';
 import {SearchInput} from '../components/SearchInput';
+import {GeoName} from '../types/geoname';
 
 export const SearchCountryScreen = ({navigation}) => {
-  const [loading, setLoading] = useState(false);
   const [query, setQuery] = useState('');
-  const [error, setError] = useState('');
+  const [result, setResult] = useState<GeoName[]>([]);
 
-  const searchCities = async () => {
+  const searchCountries = async () => {
     try {
-      setLoading(true);
       const response = await fetch(
-        'http://api.geonames.org/searchJSON?maxRows=10&username=weknowit&q=' +
+        'http://api.geonames.org/searchJSON?maxRows=10&username=weknowit&featureClass=A&q=' +
           query,
       );
       const json = await response.json();
-      const cityData = json.geonames[0];
-      if (cityData) {
-        navigation.navigate('Country', {data: cityData});
-      } else {
-        setError('No country with that name exists');
-      }
+      setResult(json.geonames);
     } catch (err) {
       console.error(err);
-    } finally {
-      setLoading(false);
     }
   };
 
+  const search = (text: string) => {
+    setQuery(text);
+    searchCountries();
+  };
+
   return (
-    <View style={{padding: 10}}>
-      <SearchInput
-        value={query}
-        onChangeText={text => setQuery(text)}
-        placeholder="Enter Country"
-        onSubmit={searchCities}
-      />
+    <View style={{padding: 10, flexDirection: 'column', flex: 1}}>
+      <View
+        style={{
+          flex: 1,
+          marginTop: 20,
+        }}>
+        <SearchInput
+          value={query}
+          onChangeText={search}
+          placeholder="Enter Country"
+          onSubmit={searchCountries}
+        />
+      </View>
+      <View style={{flex: 8}}>
+        {query === '' && result.length === 0 && (
+          <NoResult text="Start by searching for a country" />
+        )}
+        <FlatList
+          data={result}
+          keyExtractor={item => item.geonameId}
+          renderItem={({item}) => (
+            <View
+              style={{
+                marginVertical: 4,
+              }}>
+              <Button
+                title={item.name}
+                onPress={() => navigation.navigate('Country', {data: item})}
+              />
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 };
