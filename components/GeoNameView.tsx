d2a110863811f3c7cc071a0b82b69b210e23405f
Lucas Boberg <lucaspboberg@gmail.com>
2021-12-22 13:02:33 +0100

    Creates GeoNameView component

---
+++ components/GeoNameView.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+interface GeoNameViewProps {
+  name: string;
+  population: number;
+}
+
+export const GeoNameView = ({name, population}: GeoNameViewProps) => {
+  return (
+    <>
+      <Text
+        style={{
+          fontSize: 40,
+          textAlign: 'center',
+          marginTop: 30,
+        }}>
+        {name}
+      </Text>
+      <Text
+        style={{
+          fontSize: 20,
+          textAlign: 'center',
+          marginTop: 10,
+          marginBottom: 30,
+        }}>
+        has a population of{' '}
+        <Text style={{fontWeight: 'bold'}}>
+          {new Intl.NumberFormat().format(population)}
+        </Text>{' '}
+        people
+      </Text>
+    </>
+  );
+};
