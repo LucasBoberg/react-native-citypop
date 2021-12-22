caa13cb8ebe05aaa2f492daa5540cf191740de74
Lucas Boberg <lucaspboberg@gmail.com>
2021-12-22 12:46:55 +0100

    Styles CountryScreen

--- screens/CountryScreen.tsx
+++ screens/CountryScreen.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import {Button, FlatList, Text, View} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
 import {GeoName} from '../types/geoname';
+import {Button} from '../components/Button';
 
 export const CountryScreen = ({navigation, route}) => {
   const [loading, setLoading] = useState(false);
@@ -30,18 +31,53 @@ export const CountryScreen = ({navigation, route}) => {
   }, []);
 
   return (
-    <View style={{padding: 10}}>
-      <Text>{route.params.data.name}</Text>
-      <FlatList
-        data={data}
-        keyExtractor={item => item.geonameId}
-        renderItem={({item}) => (
-          <Button
-            title={item.name}
-            onPress={() => navigation.navigate('City', {data: item})}
-          />
-        )}
-      />
+    <View style={{padding: 10, flexDirection: 'column', flex: 1}}>
+      <View
+        style={{
+          flex: 1,
+        }}>
+        <Text
+          style={{
+            fontSize: 40,
+            textAlign: 'center',
+            marginTop: 30,
+          }}>
+          {route.params.data.name}
+        </Text>
+        <Text
+          style={{
+            fontSize: 20,
+            textAlign: 'center',
+            marginTop: 10,
+            marginBottom: 30,
+          }}>
+          has a population of{' '}
+          <Text style={{fontWeight: 'bold'}}>
+            {new Intl.NumberFormat().format(route.params.data.population)}
+          </Text>{' '}
+          people
+        </Text>
+      </View>
+      <View
+        style={{
+          flex: 4,
+        }}>
+        <FlatList
+          data={data}
+          keyExtractor={item => item.geonameId}
+          renderItem={({item}) => (
+            <View
+              style={{
+                marginVertical: 4,
+              }}>
+              <Button
+                title={item.name}
+                onPress={() => navigation.navigate('City', {data: item})}
+              />
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 };
