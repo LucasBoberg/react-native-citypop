732a56948cdb8bfc58bba3bbed7b129136468618
Lucas Boberg <lucaspboberg@gmail.com>
2021-12-22 12:20:30 +0100

    Styles home screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,17 +1,42 @@
 import React from 'react';
-import {Button} from 'react-native';
+import {View, Text} from 'react-native';
+import {Button} from '../components/Button';
 
 export const HomeScreen = ({navigation}) => {
   return (
-    <>
-      <Button
-        title="Search by City"
-        onPress={() => navigation.navigate('SearchCity')}
-      />
-      <Button
-        title="Search by Country"
-        onPress={() => navigation.navigate('SearchCountry')}
-      />
-    </>
+    <View style={{padding: 10}}>
+      <Text
+        style={{
+          fontSize: 40,
+          textAlign: 'center',
+          marginVertical: 30,
+        }}>
+        CityPop
+      </Text>
+      <View
+        style={{
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}>
+        <View
+          style={{
+            marginVertical: 10,
+          }}>
+          <Button
+            title="Search by City"
+            onPress={() => navigation.navigate('SearchCity')}
+          />
+        </View>
+        <View
+          style={{
+            marginVertical: 10,
+          }}>
+          <Button
+            title="Search by Country"
+            onPress={() => navigation.navigate('SearchCountry')}
+          />
+        </View>
+      </View>
+    </View>
   );
 };
